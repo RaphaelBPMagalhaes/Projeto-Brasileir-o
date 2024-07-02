13c0191432057046167716e5a1f41114b0d62d83
Raphael <rapha31.mag@gmail.com>
2024-07-02 16:01:07 -0300

    v3

--- Analise1.py
+++ Analise1.py
@@ -1,6 +1,7 @@
 import pandas as pd 
-from datetime import datetime
+from datetime import datetime, timedelta
 from matplotlib import pyplot as plt
+import os
 
 #SUBIR O DADOS EM CSV PARA 
 cartoes_doc = 'C:/Users/lucia/OneDrive/Área de Trabalho/Projeto Analista de Dados/Brasileirão/campeonato-brasileiro-cartoes.csv'
@@ -17,10 +18,11 @@ brasileirao_doc = 'C:/Users/lucia/OneDrive/Área de Trabalho/Projeto Analista de
 gols_doc = 'C:/Users/lucia/OneDrive/Área de Trabalho/Projeto Analista de Dados/Brasileirão/campeonato-brasileiro-gols.csv'
 #esse relatório contem os gols por jogador a partir do ano de 2014, contendo essas colunas: "partida_id","rodata","clube","atleta","minuto","tipo_de_gol"
 
-cartoes_df = pd.read_csv(cartoes_doc)
-partidas_df = pd.read_csv(partidas_doc)
-brasileirao_df = pd.read_csv(brasileirao_doc)
-gols_df = pd.read_csv(gols_doc)
+cartoes_df = pd.read_csv(cartoes_doc, index_col='partida_id')
+partidas_df = pd.read_csv(partidas_doc, index_col='partida_id' )
+brasileirao_df = pd.read_csv(brasileirao_doc, index_col='ID')
+brasileirao_data = pd.read_csv(brasileirao_doc, index_col='data')
+gols_df = pd.read_csv(gols_doc, index_col='partida_id')
 
 
 print(cartoes_df.head(5))
@@ -41,7 +43,7 @@ print(gols_df.dtypes)
 #CLASSIFICAÇÃO DOS DADOS. Transformando cada coluna no tipo ideal inicialmente. (Objc é uma classificação geral das variaveis mas na realidade )
             #CARTOES DF
             #no relatório de cartões trataremos todos os dados como string. Dados númericos como partida, minuto, num_camisa, são apenas para categorizar, não tendo funções matematicas aplicadas. 
-            cartoes_df['partida_id'] = cartoes_df['partida_id'].astype(str)
+            cartoes_df.index = cartoes_df.index.astype(str)
             cartoes_df['rodata'] = cartoes_df['rodata'].astype(str)
             cartoes_df['clube'] = cartoes_df['clube'].astype(str)
             cartoes_df['cartao'] = cartoes_df['cartao'].astype(str)
@@ -51,7 +53,7 @@ print(gols_df.dtypes)
             cartoes_df['minuto'] = cartoes_df['minuto'].astype(str)
 
             print(cartoes_df.dtypes)
-            print(cartoes_df['partida_id'].apply(type))
+            
 
             #PARTIDAS DF
             #no relatorio de partidas, dados estatisticos e por isso são variaveis numéricas com aplicações matematicas possiveis. 
@@ -64,8 +66,8 @@ print(gols_df.dtypes)
             partidas_df['posse_de_bola'] = partidas_df['posse_de_bola'].str.rstrip('%').astype('float') / 100.0
             partidas_df['precisao_passes'] = partidas_df['precisao_passes'].str.rstrip('%').astype('float') / 100.0
             partidas_df['faltas'] = partidas_df['faltas'].astype('int64')
-            partidas_df['cartao_amarelo '] = partidas_df['cartao_amarelo '].astype('int64')
-            partidas_df['cartao_vermelho'] = partidas_df['cartao_vermelho '].astype('int64')
+            #partidas_df['cartao_amarelo '] = partidas_df['cartao_amarelo '].astype('int64')
+            #partidas_df['cartao_vermelho'] = partidas_df['cartao_vermelho '].astype('int64')
             partidas_df['impedimentos'] = partidas_df['impedimentos'].astype('int64')
             partidas_df['escanteios'] = partidas_df['escanteios'].astype('int64')
 
@@ -74,13 +76,17 @@ print(gols_df.dtypes)
 
             #BRASILEIRAO DF
 
-            brasileirao_df['ID'] = brasileirao_df['ID'].astype(str)
+            brasileirao_df.index = brasileirao_df.index.astype(str)
             brasileirao_df['rodata'] = brasileirao_df['rodata'].astype(str)
+            brasileirao_df['data'] = brasileirao_df['data'].astype(str)
+            brasileirao_df['data'] = pd.to_datetime(brasileirao_df['data'], format='%d/%m/%Y')
 
-            brasileirao_df['data'] = pd.to_datetime(brasileirao_df['data'], dayfirst=True)
-            #brasileirao_df['hora'] = pd.to_datetime(brasileirao_df['hora'], format='%H:%M').dt.time
+                brasileirao_df['hora'] = pd.to_datetime(brasileirao_df['hora'], format='%H:%M', dayfirst= False)
+
+       #brasileirao_df['hora'] = pd.to_datetime(brasileirao_df['hora'], format='%H:%M').dt.time
             #converti a data para uma variavel de tempo para ficar mais facil de utilizar mas a variavel de hora e minuto estava dando erro, acredito que por conta da falta de dados. 
             print(brasileirao_df.dtypes)
+
 
             #GOLS DF
             gols_df['partida_id'] = gols_df['partida_id'].astype(str)
@@ -121,9 +127,66 @@ print(gols_df.dtypes)
         gols_por_atleta = filtro_gols['atleta'].value_counts()
         gols_por_atleta_df = gols_por_atleta.reset_index()
         gols_por_atleta_df.columns = ['atleta', 'frequencia']
-        gols_por_atleta_df = gols_por_atleta_df.sort_values(by='frequencia',ascending = false)
+        gols_por_atleta_df = gols_por_atleta_df.sort_values(by='frequencia',ascending = False)
         # Identificar o atleta com mais gols
         print(gols_por_atleta_df)
+
+
+        #contar gols por time
+        gols_por_time = filtro_gols['clube'].value_counts()
+        gols_por_time_df = gols_por_time.reset_index()
+        gols_por_time_df.columns = ['clube', 'frequencia']
+        gols_por_time_df = gols_por_time_df.sort_values(by='frequencia', ascending=False)
+        print(gols_por_time_df)
+
+        #minuto que mais saiu gol
+        gols_minuto = filtro_gols['minuto'].value_counts()
+        gols_minuto_df = gols_minuto.reset_index()
+        gols_minuto_df.columns = ['minuto', 'frequencia']
+        gols_minuto_df = gols_minuto_df.sort_values(by='frequencia', ascending=False)
+        print(gols_minuto_df)
+
+#HORA MAIS PROVAVEL DE UM JOGO ONDE O MANDANTE SAIA VENCEDOR
+df_vencedor_mandante = brasileirao_df[brasileirao_df['vencedor'] == brasileirao_df['mandante']]
+
+# Analisar a distribuição das horas dos jogos vencedores
+hora_vencedor_mandante = df_vencedor_mandante['hora'].value_counts().sort_index()
+
+# Encontrar a hora mais provável
+hora_mais_provavel = hora_vencedor_mandante.idxmax()
+
+# Exibir o resultado
+print(f"A hora mais provável do mandante sair vencedor é: {hora_mais_provavel}")
+
+
+#TABELA PARA DETERMINAR QUE HORA É MAIS PROVAVEL DE SAIR UM VENCEDOR 
+                brasileirao_df['resultado'] = brasileirao_df.apply(lambda row: 'Vencedor' if row['vencedor'] == row['mandante'] else ('Perdedor' if row['vencedor'] == row['visitante'] else 'Empate'), axis=1)
+
+# Calcular o número total de jogos para cada hora
+total_jogos_por_hora = brasileirao_df['hora'].value_counts().sort_index()
+
+# Calcular o número de vitórias, derrotas e empates do mandante para cada hora
+vitorias_por_hora = brasileirao_df[brasileirao_df['resultado'] == 'Vencedor']['hora'].value_counts().sort_index()
+derrotas_por_hora = brasileirao_df[brasileirao_df['resultado'] == 'Perdedor']['hora'].value_counts().sort_index()
+empates_por_hora = brasileirao_df[brasileirao_df['resultado'] == 'Empate']['hora'].value_counts().sort_index()
+
+# Calcular os percentuais de vitórias, derrotas e empates para cada hora
+percentual_vitorias = (vitorias_por_hora / total_jogos_por_hora * 100).fillna(0)
+percentual_derrotas = (derrotas_por_hora / total_jogos_por_hora * 100).fillna(0)
+percentual_empates = (empates_por_hora / total_jogos_por_hora * 100).fillna(0)
+
+# Criar uma tabela com esses percentuais
+tabela_percentuais = pd.DataFrame({
+    'Percentual_Vitorias': percentual_vitorias,
+    'Percentual_Derrotas': percentual_derrotas,
+    'Percentual_Empates': percentual_empates
+}).fillna(0)
+
+# Exibir a tabela
+tbl_percents = tabela_percentuais.resample('1H').agg({'Percentual_Derrotas':'mean', 'Percentual_Empates':'mean', 'Percentual_Vitorias':'mean'})
+print(tbl_percents.sort_values(by='hora', ascending = True))
+
+
 
 #time que mais venceu jogos desde 2003:
         vitorias_por_time = brasileirao_df['vencedor'].value_counts()
@@ -153,5 +216,8 @@ brasileirao_df.rename(columns={'ID':'partida_id'}, inplace=True)
 
 #os primeiros dados que vou juntar é o das partidas com os dados do brasileirao, pois ambas tem dados a partir de 2003 e isso vai aumentar as informações validas nos tipos de dados. 
 
-brasileirao_completo_df = pd.merge(brasileirao_df,partidas_df, on="partida_id", how='inner')
+#brasileirao_completo_df = pd.merge(brasileirao_df,partidas_df, on="partida_id", how='inner')
+brasileirao_completo_df = pd.merge(partidas_df,brasileirao_df, on="partida_id", how='inner')
 print(brasileirao_completo_df)
+
+os.getcwd()
--- Readme.md
+++ Readme.md
@@ -5,6 +5,15 @@ O estudo consiste na verificação de dados totais, na junção de tabelas e no 
 Inicialmente se ve que tem dados a partir de 2003 mas inúmeros dados faltantes até o inicio de 2014. Assim podemos usar os dados apartir 2003 para ter conclusões gerais e depois utilizar os dados a partir de 2014 para ter conclusões mais detalhadas. Por exemeplo: podemos saber o time que mais venceu, o estádio que mais teve jogos e etc. a partir de 2003 mas não podemos saber a escalação mais utilizada, o número de gols, faltas, cartões e dados mais detalhados pois esses só estaram completos e presentes depois de 2014. 
 
 Quem foi o maior artilheiro de 2014-2023? 
+
 Maior marcador de penaltis 14-23? 
+
 Maior marcdor de gols contra? 
+
 Time mais vencedor a partir de 2003? 
+
+
+Time com maior frequênica no campeonato
+Times que mais jogaram um contra o outro
+Qual posição é mais propensa a tomar cartões? 
+Média de posse de bola por time
